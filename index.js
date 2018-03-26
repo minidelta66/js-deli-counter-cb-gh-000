4dc07acd85a676172d6af004fbee88e0c8e4b3c4
Andrzej Nicpoń <andrzejmd66@gmail.com>
2018-03-26 18:12:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
-  return 'Wlcome, ' + name + '. You are number ' + katzDeliLine.length + ' in line.';
-  
+  return 'Welcome, ' + name + '. You are number ' + katzDeliLine.length + ' in line.';
+
 }
