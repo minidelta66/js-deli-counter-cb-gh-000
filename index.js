3eeb501fe57ff4c91fdbee231200f2d5a0356be3
Andrzej Nicpoń <andrzejmd66@gmail.com>
2018-03-26 18:23:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function nowServing() {
   var person = katzDeliLine[0];
   katzDeliLine.shift[0];
   if ((katzDeliLine.length + 1) > 0) {
-  return 'Currently serving ' + person;}
+  return 'Currently serving ' +katzDeliLine[0];}
   else {
     return "There is nobody waiting to be served!";
   }
