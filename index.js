e95e739c9df1eaaeb74013930c8cf8fe8d4c20b0
Andrzej Nicpoń <andrzejmd66@gmail.com>
2018-03-26 18:37:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function nowServing(katzDeliLine) {
 
 function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
-    return "The line is currently empty";
+    return "The line is currently empty.";
   }
 
   for (var i = 0; i < katzDeliLine.length; i = i+1) {
